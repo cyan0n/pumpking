3b5e655a2c685492113f64956f69661f16d2e5d3
Cyan0n <cyan0n.dev@gmail.com>
2020-10-27 22:20:17 +0100

    Updated permissions

--- public/js/app.js
+++ public/js/app.js
@@ -33979,7 +33979,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /mnt/c/Users/Thomas/workspace/Unity/september/pumpking/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/dragon/projects/pumpkin_king/resources/js/app.js */"./resources/js/app.js");
 
 
 /***/ })
